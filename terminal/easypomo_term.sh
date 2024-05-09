2a600f49c3b403e4fd95acf5f3b60371d799694b
Rodrigo Ceccato <rodrigoceccatodefreitas@gmail.com>
2024-05-09 14:16:04 -0300

    Add option to use libnotify

--- terminal/easypomo_term.sh
+++ terminal/easypomo_term.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 url='https://easypomo.duckdns.org:443/json-endpoint'
+state_file='/tmp/easypomo_state'
 
 # Function to get online step
 get_online_step() {
@@ -24,14 +25,35 @@ get_online_step() {
         if [[ $is_long_resting == "true" ]]; then
             remaining_time=$((long_rest_time - curr_time))
             status_icon="🚬☕"
+            new_state="long_resting"
         else
             remaining_time=$((short_rest_time - curr_time))
             status_icon="☕"
+            new_state="short_resting"
         fi
     else
         # For focus time, this logic remains unchanged
         remaining_time=$curr_time
         status_icon="⌛"
+        new_state="working"
+    fi
+
+    # Notify only if the state has changed
+    if [[ $notify == "true" ]]; then
+        if [[ ! -f $state_file ]] || [[ $(cat $state_file) != $new_state ]]; then
+            case $new_state in
+                "working")
+                    notify-send "Time to work!" "Focus on your tasks."
+                    ;;
+                "short_resting")
+                    notify-send "Time to rest!" "Take a short break."
+                    ;;
+                "long_resting")
+                    notify-send "Time to rest!" "Take a long break."
+                    ;;
+            esac
+            echo "$new_state" > "$state_file"
+        fi
     fi
 
     # Format remaining_time as MM:SS with leading zeros
@@ -39,13 +61,12 @@ get_online_step() {
     seconds=$((remaining_time % 60))
     formatted_time=$(printf '%02d:%02d' $minutes $seconds)
 
-    # If is not resting, and not long resting, increment pomo_count by 1
+    # If not resting, increment pomo_count by 1
     if [[ $is_resting == "false" ]] && [[ $is_long_resting == "false" ]]; then
         display_pomo_count=$((pomo_count + 1))
     else
         display_pomo_count=$pomo_count
     fi
-
 
     status_string="${display_pomo_count}${status_icon} ${formatted_time}"
 
@@ -55,6 +76,12 @@ get_online_step() {
 
     echo "$status_string"
 }
+
+# Check for --notify option
+notify="false"
+if [[ $1 == "--notify" ]]; then
+    notify="true"
+fi
 
 # Call the function
 get_online_step
